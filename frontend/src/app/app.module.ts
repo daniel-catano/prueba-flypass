594844f26548caa6d4959e55fc003f9157b652f7
dcatanor <dasniel199410@gmail.com>
2024-02-02 15:30:42 -0500

    Primer commit del front

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
